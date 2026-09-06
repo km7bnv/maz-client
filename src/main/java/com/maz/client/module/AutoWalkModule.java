cab0410d168ef81c2e3d4970e529a98b0e2e0ff8
Parsley Ohxi <km7bnv@proton.me>
2026-09-06 14:12:13 -0700

    Add AutoWalk module

---
+++ src/main/java/com/maz/client/module/AutoWalkModule.java
@@ -0,0 +1,23 @@
+package com.maz.client.module;
+
+import net.minecraft.client.Minecraft;
+
+public class AutoWalkModule extends Module {
+
+    public AutoWalkModule() {
+        super("AutoWalk", ModuleCategory.UTILITY);
+    }
+
+    @Override
+    public void onTick() {
+        Minecraft client = Minecraft.getInstance();
+        if (client.player != null && client.screen == null) {
+            client.options.keyUp.setDown(true);
+        }
+    }
+
+    @Override
+    protected void onDisable() {
+        Minecraft.getInstance().options.keyUp.setDown(false);
+    }
+}
