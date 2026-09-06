5f7eb9d919c625602a933464dd666d8ad859e0ac
Parsley Ohxi <km7bnv@proton.me>
2026-09-06 13:32:48 -0700

    Add FPS booster module

---
+++ src/main/java/com/maz/client/module/FpsBoosterModule.java
@@ -0,0 +1,8 @@
+package com.maz.client.module;
+
+public class FpsBoosterModule extends Module {
+
+    public FpsBoosterModule() {
+        super("FPS Booster", ModuleCategory.PERFORMANCE);
+    }
+}
