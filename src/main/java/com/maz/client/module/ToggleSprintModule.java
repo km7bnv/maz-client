b42ceede9f8eec0fdfdabf15deb22c10d82d85cc
Parsley Ohxi <km7bnv@proton.me>
2026-09-06 14:31:56 -0700

    Fix ToggleSprint for Minecraft 26.2

--- src/main/java/com/maz/client/module/ToggleSprintModule.java
+++ src/main/java/com/maz/client/module/ToggleSprintModule.java
@@ -11,7 +11,7 @@ public class ToggleSprintModule extends Module {
     @Override
     public void onTick() {
         Minecraft client = Minecraft.getInstance();
-        if (client.player != null && client.screen == null) {
+        if (client.player != null) {
             boolean movingForward = client.options.keyUp.isDown();
             client.options.keySprint.setDown(movingForward);
         }
