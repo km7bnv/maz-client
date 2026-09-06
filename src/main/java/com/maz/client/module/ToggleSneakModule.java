85ed74b5125d3736d0935cafda152b980b876fa7
Parsley Ohxi <km7bnv@proton.me>
2026-09-06 14:59:05 -0700

    Add ToggleSneak utility module

---
+++ src/main/java/com/maz/client/module/ToggleSneakModule.java
@@ -0,0 +1,23 @@
+package com.maz.client.module;
+
+import net.minecraft.client.Minecraft;
+
+public class ToggleSneakModule extends Module {
+
+    public ToggleSneakModule() {
+        super("ToggleSneak", ModuleCategory.UTILITY);
+    }
+
+    @Override
+    public void onTick() {
+        Minecraft client = Minecraft.getInstance();
+        if (client.player != null && client.screen == null) {
+            client.options.keyShift.setDown(true);
+        }
+    }
+
+    @Override
+    protected void onDisable() {
+        Minecraft.getInstance().options.keyShift.setDown(false);
+    }
+}
