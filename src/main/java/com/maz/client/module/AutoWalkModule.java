cd76ecbde89597892b7121b005ea49e919e66315
Parsley Ohxi <km7bnv@proton.me>
2026-09-06 14:30:42 -0700

    Fix AutoWalk for Minecraft 26.2

--- src/main/java/com/maz/client/module/AutoWalkModule.java
+++ src/main/java/com/maz/client/module/AutoWalkModule.java
@@ -11,7 +11,7 @@ public class AutoWalkModule extends Module {
     @Override
     public void onTick() {
         Minecraft client = Minecraft.getInstance();
-        if (client.player != null && client.screen == null) {
+        if (client.player != null) {
             client.options.keyUp.setDown(true);
         }
     }
