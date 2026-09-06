6c1ad5289e1acd4446ccfc501582182c78606b68
Parsley Ohxi <km7bnv@proton.me>
2026-09-06 14:12:08 -0700

    Add generic simple module type

---
+++ src/main/java/com/maz/client/module/SimpleModule.java
@@ -0,0 +1,8 @@
+package com.maz.client.module;
+
+public class SimpleModule extends Module {
+
+    public SimpleModule(String name, ModuleCategory category) {
+        super(name, category);
+    }
+}
