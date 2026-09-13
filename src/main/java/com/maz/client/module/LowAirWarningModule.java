e84e423e0121f75252d6ecdf93bd5b946b724f41
Parsley Ohxi <km7bnv@proton.me>
2026-09-12 23:44:05 -0700

    Add low air warning module skeleton

---
+++ src/main/java/com/maz/client/module/LowAirWarningModule.java
@@ -0,0 +1,7 @@
+package com.maz.client.module;
+
+public final class LowAirWarningModule extends Module {
+    public LowAirWarningModule() {
+        super("Low Air Warning", "Warns locally when air is running low.", ModuleCategory.UTILITY);
+    }
+}
